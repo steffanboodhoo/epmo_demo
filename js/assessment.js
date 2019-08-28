afc73bf225cc381810bbd9d506747dae8002ae30
steffanboodhoo <steffan_boodhoo@hotmail.com>
2019-08-28 12:45:18 -0400

    added charts to project page

--- js/assessment.js
+++ js/assessment.js
@@ -4,18 +4,35 @@ window.onload = () => {
     const PROJECT = params.get('title');
     document.getElementById('project-title').append(PROJECT);
 
-    window.setTimeout(()=>{
-        document.getElementById('assessment-design_quality-val').innerText=document.getElementById('assessment-design_quality').value;
-        document.getElementById('assessment-delivery_quality-val').innerText=document.getElementById('assessment-delivery_quality').value;
-        document.getElementById('assessment-execution_quality-val').innerText=document.getElementById('assessment-execution_quality').value;
-    },0)
+    window.setTimeout(() => {
+        document.getElementById('assessment-design_quality-val').innerText = document.getElementById('assessment-design_quality').value;
+        document.getElementById('assessment-delivery_quality-val').innerText = document.getElementById('assessment-delivery_quality').value;
+        document.getElementById('assessment-execution_quality-val').innerText = document.getElementById('assessment-execution_quality').value;
+    }, 0)
     document.getElementById('assessment-design_quality').addEventListener('change', ev => {
-        document.getElementById('assessment-design_quality-val').innerText=ev.target.value;
+        document.getElementById('assessment-design_quality-val').innerText = ev.target.value;
     })
     document.getElementById('assessment-delivery_quality').addEventListener('change', ev => {
-        document.getElementById('assessment-delivery_quality-val').innerText=ev.target.value;
+        document.getElementById('assessment-delivery_quality-val').innerText = ev.target.value;
     })
     document.getElementById('assessment-execution_quality').addEventListener('change', ev => {
-        document.getElementById('assessment-execution_quality-val').innerText=ev.target.value;
+        document.getElementById('assessment-execution_quality-val').innerText = ev.target.value;
     })
+
+    document.getElementById('assessment_submit').addEventListener('click', ev => {
+        ev.preventDefault();
+        const design = parseInt(document.getElementById('assessment-design_quality').value), delivery = parseInt(document.getElementById('assessment-delivery_quality').value), execution = parseInt(document.getElementById('assessment-delivery_quality').value);
+        const overall = (design + delivery + execution) / 3.0;
+        let data = Cookies.getJSON(PROJECT);
+        console.log(data);
+        if (data === undefined) {
+            data = { count:1, average: overall, score: [{ design, delivery, execution }] }
+        }else{
+            const new_average = (data.count * data.average + overall) / (data.count + 1)
+            data.count += 1, data.score.push({ design, delivery, execution }), data.average=new_average;
+        }
+        console.log(data);
+        Cookies.set(PROJECT, data);
+    })
+
 }
--- js/project.js
+++ js/project.js
@@ -10,4 +10,38 @@ window.onload = () => {
         window.location = `${window.location.origin}/assessment.html?title=${PROJECT}`
     })
 
+    //setup charts
+    const budget_dataset = [40000, 80000, 80000, 300000, 300000, 300000, 380000], progress_dataset = [0, 35, 45, 50, 60, 70, 75], labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+    const options = {
+        title: {
+            display: true,
+            text: ''
+        }
+    }
+    const create_chart = (elem_id, data, options) => {
+        var ctx = document.getElementById(elem_id).getContext('2d');
+        var chart = new Chart(ctx, {
+            // The type of chart we want to create
+            type: 'line',
+
+            // The data for our dataset
+            data: {
+                labels: labels,
+                datasets: [
+                    {
+                        label: PROJECT,
+                        backgroundColor: 'rgb(255, 99, 132)',
+                        borderColor: 'rgb(255, 99, 132)',
+                        data: data
+                    }
+                ]
+            },
+
+            // Configuration options go here
+            options: options,
+        });
+    }
+    create_chart('chart_1', budget_dataset, {text:'Budget over time', ...options});
+    create_chart('chart_2', progress_dataset, {text:'Progress over time', ...options});
+
 }
